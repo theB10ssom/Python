7ada6a3da2981ffdfe898e1bc8d7bfaad35a5fa4
Yongmin Kwon <46968697+YongminKwon@users.noreply.github.com>
2021-04-08 02:03:41 +0900

    Create 2839.py

---
+++ baekjoon/2839.py
@@ -0,0 +1,19 @@
+N = int(input('enter'))
+
+while N > 0:
+  
+    x, y = 0, 0
+    lim_x = N // 3
+    lim_y = N // 5
+
+    result = []
+    for y in range(lim_y + 1):
+        for x in range(lim_x + 1):
+            cal = (3 * x) + (5 * y) == N
+            if cal == True:
+                result.append(x+y)
+
+    if len(result) >= 1:
+        print(min(result))
+    else:
+        print('-1')
