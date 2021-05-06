8e04172c51ac4d473143dc1c41094a6a1b117d39
Yongmin Kwon <46968697+YongminKwon@users.noreply.github.com>
2021-05-06 23:02:55 +0900

    Add files via upload

---
+++ baekjoon/1110.py
@@ -0,0 +1,12 @@
+N = input()
+if int(N) < 10:
+    N = '0' + N
+
+i = 0
+while True:
+    result = int(N[-2]) + int(N[-1])
+    N += str(result)[-1]
+    i += 1
+    if N[:2] == N[-2:]:
+        break
+print(i)
