7f44f1ef99c7f1be3755795a5baae6622536a772
Yongmin Kwon <46968697+YongminKwon@users.noreply.github.com>
2021-05-06 23:04:49 +0900

    Create 2884.py

---
+++ baekjoon/2884.py
@@ -0,0 +1,9 @@
+H, M = map(int, input().split())
+
+if M >= 45:
+    print(f'{H} {M - 45}')
+else: # 45분을 빼면 시간을 빼야하는 경우
+    if H > 0:
+        print(f'{H - 1} {(60 + M) - 45}')
+    else: #0시인 경우
+        print(f'23 {(60 + M) - 45}')
